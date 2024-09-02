35c5fbfeff05e391e13e431153d5c9105ec98750
luizakp <luiza.puccinelli06@gmail.com>
2024-09-02 12:23:16 -0300

    chore: use subgraph on UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { AttestionList } from "@/components/AttestionList";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
