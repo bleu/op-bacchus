ad43498a27b413934e32a56cd0026b07e4833b94
luizakp <luiza.puccinelli06@gmail.com>
2024-09-02 12:22:47 -0300

    refactor: codegen

--- package/gql/codegen.ts
+++ package/gql/codegen.ts
@@ -1,78 +1,18 @@
-import { CodegenConfig } from "@graphql-codegen/cli";
-
-function capitalize(word: string) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-
-export enum Network {
-    Optimism = "optimism",
-    OptimismSepolia = "optimism-sepolia",
-  }
-
-export enum Subgraph {
-  EasAttestations = "eas",
-}
-
-export const SUBGRAPHS = {
-  [Subgraph.EasAttestations]: {
-    name: Subgraph.EasAttestations,
-    endpoints() { 
-
-      return {
-        [Network.Optimism]: "https://optimism.easscan.org/graphql",
-        [Network.OptimismSepolia]: "https://optimism-sepolia.easscan.org/graphql",
-      };
-    },
-    endpointFor(network: Network) {
-      return this.endpoints()[network];
-    },
-  },
-};
-
-const generates = Object.assign(
-  {},
-  ...Object.values(SUBGRAPHS).map(({ name, endpoints }) =>
-    Object.fromEntries(
-      Object.entries(endpoints())
-        .map(([network, endpoint]) => [
-          [
-            `./src/${name}/__generated__/${capitalize(network)}.ts`,
-            {
-              schema: endpoint,
-              documents: [`src/${name}/*.ts`],
-              plugins: [
-                "typescript",
-                "typescript-operations",
-                "typescript-graphql-request",
-                "plugin-typescript-swr",
-              ],
-            },
-          ],
-          [
-            `./src/${name}/__generated__/${capitalize(network)}.server.ts`,
-            {
-              schema: endpoint,
-              documents: [`src/${name}/*.ts`],
-              plugins: [
-                "typescript",
-                "typescript-operations",
-                "typescript-graphql-request",
-              ],
-            },
-          ],
-        ])
-        .flat(1),
-    ),
-  ),
-);
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  config: {
-    autogenSWRKey: true,
-    enumsAsTypes: true,
-    futureProofEnums: true,
+  overwrite: true,
+  schema: "https://optimism-sepolia.easscan.org/graphql",
+  documents: "src/**/*.(ts|tsx)",
+  generates: {
+    "src/eas/generated.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-graphql-request",
+      ],
+    },
   },
-  generates,
 };
 
 export default config;
--- package/gql/src/eas/index.ts
+++
@@ -1,9 +0,0 @@
-import { Network } from "../../codegen";
-
-import { getSdkWithHooks as optimismSdk } from "./__generated__/Optimism";
-import { getSdkWithHooks as optimismSepoliaSdk } from "./__generated__/Optimism-sepolia";
-
-export default {
-  [Network.Optimism]: optimismSdk,
-  [Network.OptimismSepolia]: optimismSepoliaSdk,
-};
