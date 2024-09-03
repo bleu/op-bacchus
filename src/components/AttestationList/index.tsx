a524d0161845a128bd3e62818c104f8b1a72a214
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-03 09:47:37 -0300

    fix: attestationsList typing

--- src/components/AttestationList/index.tsx
+++ src/components/AttestationList/index.tsx
@@ -4,9 +4,18 @@ import { useSigner } from "@/hooks/useSigner";
 import { attestationsApi } from "../../../package/gql/src/eas/sdk";
 import { parseData } from "./utils";
 
+interface AttestationData {
+  attestations: Array<{
+    id: string;
+    decodedDataJson: string;
+  }>;
+}
+
 export function AttestationList() {
   const signer = useSigner();
-  const [attestationData, setAttestationData] = useState(null);
+  const [attestationData, setAttestationData] = useState<AttestationData>({
+    attestations: [],
+  } as AttestationData);
 
   useEffect(() => {
     async function fetchAttestationData() {
@@ -31,11 +40,11 @@ export function AttestationList() {
     return <div>Loading attestation data...</div>;
   }
 
+  console.log(attestationData);
   return (
     <>
       <h2>Attestations</h2>
       <div>
-        {/* @ts-ignore */}
         {attestationData.attestations.map((data: any) => (
           <AttestationItem key={data.id} data={data.decodedDataJson} />
         ))}
@@ -46,6 +55,7 @@ export function AttestationList() {
 
 function AttestationItem({ data }: { data: any }) {
   const parsedData = parseData(data);
+  console.log(parsedData);
 
   return (
     <div>
