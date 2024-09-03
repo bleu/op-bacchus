2214202c31a858e063d6c797692c90d70642759a
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-03 09:08:46 -0300

    fix: move attestation list to events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { AttestationList } from "@/components/AttestionList";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
