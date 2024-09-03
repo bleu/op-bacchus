87e4032b7286286368509e473425dbe496fe818a
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-03 09:03:34 -0300

    fix: name attestion -> attestation

--- package/gql/src/eas/sdk.ts
+++ package/gql/src/eas/sdk.ts
@@ -1,9 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated";
 
-
 export const ENDPOINT = "https://optimism-sepolia.easscan.org/graphql";
 
 const client = new GraphQLClient(ENDPOINT);
 
-export const attestionsApi = getSdk(client);
+export const attestationsApi = getSdk(client);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import { AttestionList } from "@/components/AttestionList";
+import { AttestationList } from "@/components/AttestionList";
 
 export default function Home() {
-
-
-
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Bacchus Onboarding</h1>
-        <AttestionList />
-        {/* <h1>Create Party</h1>
-        <form>
-          <textarea placeholder="Party name" />
-          <textarea placeholder="Owner address" />
-        </form> */}
-  
-      </main>
-    );
-  }
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Bacchus Onboarding</h1>
+      <AttestationList />
+    </main>
+  );
+}
