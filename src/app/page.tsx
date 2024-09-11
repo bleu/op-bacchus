0f6564f1fdccd3664bfc6c3b4d5e9f580b9d3810
Gabriel Orleans <gabrielorleans@Gabriels-MacBook-Air.local>
2024-09-11 08:39:42 -0300

    Create ticket schema and button to register it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+import { CreateTicketSchemaButton } from "@/components/CreateTicketSchemaButton";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Bacchus Onboarding</h1>
+      <CreateTicketSchemaButton />
     </main>
   );
 }
