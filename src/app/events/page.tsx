1c3ca5660047da5a3a908c5e83a2fb2f3fee842c
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-11 09:53:55 -0300

    feat: events in /events are now clickable to enter event details

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -6,6 +6,7 @@ import { API_URL_MAPPING } from "@/lib/gqlEasAttestation";
 import { useChainId } from "wagmi";
 import { useMemo } from "react";
 import { parseEventsData } from "./parseEventsData";
+import Link from "next/link";
 
 export default function Events() {
   const signer = useSigner();
@@ -26,7 +27,11 @@ export default function Events() {
     () =>
       data?.attestations &&
       data.attestations.map((data: any) => (
-        <AttestationItem key={data.id} data={data.decodedDataJson} />
+        <AttestationItem
+          key={data.id}
+          id={data.id}
+          data={data.decodedDataJson}
+        />
       )),
     [data?.attestations]
   );
@@ -42,28 +47,42 @@ export default function Events() {
   return (
     <div className="mx-20">
       <h2>Attestations</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">{attestationList}</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {attestationList}
+      </div>
     </div>
   );
 }
 
-function AttestationItem({ data }: { data: any }) {
+function AttestationItem({ id, data }: { id: string; data: any }) {
   const parsedData = parseEventsData(data);
 
   return (
-    <div className="border-2 rounded-lg w-60">
+    <Link
+      href={`/event/${id}`}
+      className="border-2 rounded-lg w-60 hover:border-slate-600"
+    >
+      {/* <div className=""> */}
       <div className="p-3">
         <h1 className="text-base font-bold">{parsedData.name}</h1>
         <h2 className="text-xs">{parsedData.startsAt}</h2>
       </div>
       <div>
-        <img className="w-full h-auto" alt="Template image" src={parsedData.imageUrl} />
+        <img
+          className="w-full h-auto"
+          alt="Template image"
+          src={parsedData.imageUrl}
+        />
       </div>
       <div className="p-3">
-        <h2 className="text-xs text-gray-700 mb-4">Brief description: {parsedData.briefDescription}</h2>
-        <h2 className="text-xs text-gray-700 mb-4">Full description: {parsedData.fullDescription}</h2>
+        <h2 className="text-xs text-gray-700 mb-4">
+          Brief description: {parsedData.briefDescription}
+        </h2>
+        <h2 className="text-xs text-gray-700 mb-4">
+          Full description: {parsedData.fullDescription}
+        </h2>
       </div>
-    </div>
-
+      {/* </div> */}
+    </Link>
   );
 }
