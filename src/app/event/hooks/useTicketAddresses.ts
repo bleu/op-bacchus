0d8d0e593ead1f13b7f99321e6a1a2cbae5c9c64
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-16 19:49:05 -0300

    feat: add ticket real data to event details

---
+++ src/app/event/hooks/useTicketAddresses.ts
@@ -0,0 +1,44 @@
+import { GET_TICKETS_BY_EVENT_QUERY } from "@/lib/gqlEasAttestation/query";
+import { useQuery } from "urql";
+import { API_URL_MAPPING } from "@/lib/gqlEasAttestation";
+import { useMemo } from "react";
+import { useChainId } from "wagmi";
+import { parseEventsData } from "@/app/events/parseEventsData";
+import { CREATE_TICKET_SCHEMA_UID } from "@/hooks/useCreateTicketMultiAttestations";
+import type { Event } from "@/hooks/useCreateEventAttestation";
+
+export const useTicketAddresses = ({
+  eventId,
+  eventData,
+}: {
+  eventId: string;
+  eventData: Event | null | undefined;
+}) => {
+  const chainId = useChainId();
+  const [result] = useQuery({
+    query: GET_TICKETS_BY_EVENT_QUERY,
+    variables: { schemaId: CREATE_TICKET_SCHEMA_UID, eventId },
+    context: useMemo(() => ({ url: API_URL_MAPPING[chainId] }), [chainId]),
+    pause: !eventData,
+  });
+
+  const { data, fetching, error } = result;
+
+  const ticketAddresses = useMemo(
+    () =>
+      data?.attestations &&
+      data.attestations.map((ticket) => {
+        return ticket.recipient;
+      }),
+    [data?.attestations]
+  );
+
+  // WILL BE USED IN THE FUTURE
+  // const ticketsInternalData = useMemo(
+  //   () =>
+  //     data?.attestations && parseTicketsData(data.attestations?.decodedDataJson),
+  //   [data?.attestations]
+  // );
+
+  return { ticketAddresses, fetching, error };
+};
