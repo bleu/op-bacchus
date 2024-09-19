36e432bfa6dbca0d3c2dde91b1ba020196d87ae0
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-19 08:18:49 -0300

    feat: add QR Code with access data to ticket

--- src/app/event/components/UserTicket.tsx
+++ src/app/event/components/UserTicket.tsx
@@ -1,8 +1,8 @@
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Copy, Check } from "lucide-react";
 import clsx from "clsx";
 import { useState } from "react";
 import type { Event } from "@/hooks/useCreateEventAttestation";
+import QRCode from "react-qr-code";
+import { MaterialSymbol } from "react-material-symbols";
 
 export type TicketInfoType =
   | {
@@ -13,6 +13,40 @@ export type TicketInfoType =
     }
   | undefined;
 
+const ButtonCopyToClipBoard = ({
+  contentToCopy,
+  buttonText,
+  className,
+}: {
+  contentToCopy: string;
+  buttonText: string;
+  className: string;
+}) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = async (text: string) => {
+    await navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <button
+      onClick={() => copyToClipboard(contentToCopy)}
+      className={className}
+    >
+      {copied ? (
+        <MaterialSymbol icon="check" size={18} />
+      ) : (
+        <MaterialSymbol icon="content_copy" size={18} />
+      )}
+      {buttonText}
+    </button>
+  );
+};
+
 const TruncateAndCopyText = ({
   text,
   className,
@@ -22,13 +56,6 @@ const TruncateAndCopyText = ({
   maxWidth: number;
   className?: string;
 }) => {
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = () => {
-    setCopied(true);
-    setTimeout(() => setCopied(false), 1000);
-  };
-
   return (
     <div className="flex items-center space-x-2">
       <p
@@ -41,18 +68,11 @@ const TruncateAndCopyText = ({
       >
         {text}
       </p>
-      <CopyToClipboard text={text} onCopy={handleCopy}>
-        <button
-          className="p-1 rounded hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          aria-label="Copy to clipboard"
-        >
-          {copied ? (
-            <Check className="w-4 h-4 text-green-500" />
-          ) : (
-            <Copy className="w-4 h-4" />
-          )}
-        </button>
-      </CopyToClipboard>
+      <ButtonCopyToClipBoard
+        className="p-1 rounded hover:bg-gray-200 focus:outline-none"
+        contentToCopy={text}
+        buttonText=""
+      />
     </div>
   );
 };
@@ -90,8 +110,8 @@ export const UserTicket = ({
     : { date: undefined, time: undefined };
 
   return (
-    <div className="flex items-start justify-start w-full border-2 rounded-3xl p-8 mt-4">
-      <div key="qrcode" className="w-48 h-48 bg-slate-600 flex-none"></div>
+    <div className="flex items-start justify-start w-full border-2 rounded-3xl p-8 mt-8">
+      <QRCode className="w-48 h-48 flex-none" value={eventData?.access || ""} />
       <div className="flex flex-col justify-center items-start w-full pr-2 pl-2 pt-0 ml-8">
         <div className="flex items-center justify-between w-full pb-4 border-b-2 border-dashed">
           <h1 className="text-2xl font-bold">{eventData?.name}</h1>
@@ -125,9 +145,11 @@ export const UserTicket = ({
           </div>
         </div>
         <div className="w-full flex items-center justify-center">
-          <button className="bg-red-600 rounded-xl mt-6 px-16 py-2 text-white hover:bg-red-800">
-            View Event Access
-          </button>
+          <ButtonCopyToClipBoard
+            className="flex justify-center items-center gap-2 bg-gray-600 rounded-xl mt-5 px-16 py-2 text-white hover:bg-gray-800"
+            contentToCopy={eventData?.access || ""}
+            buttonText="Copy access to clipboard"
+          />
         </div>
       </div>
     </div>
