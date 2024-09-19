5dbfef56ba97d5f1143be1c10cb1248bb4280ca1
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-19 18:34:56 -0300

    feat: insert loading and fix nav typo

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,6 +7,7 @@ import { USER_ATTESTATIONS_QUERY } from "@/lib/gqlEasAttestation/query";
 import { useMemo } from "react";
 import { useQuery } from "urql";
 import { useChainId } from "wagmi";
+import { LoadingIndicator } from "@/components/LoadingIndicator";
 
 interface DataEntry {
   attester: string;
@@ -49,7 +50,7 @@ export default function Events() {
     pause: !signer,
   });
 
-  const { data } = result;
+  const { data, fetching } = result;
 
   const attestationList = useMemo(
     () =>
@@ -64,13 +65,14 @@ export default function Events() {
         : [],
     [data?.attestations],
   );
+  
 
-  if (!signer) {
-    return <div>Connect a wallet to view your attestations.</div>;
+  if (!signer && !fetching) {
+    return <div className="flex items-center justify-center h-screen -mt-36">Connect a wallet to view your attestations.</div>;
   }
 
-  if (!data) {
-    return <div>Loading attestation data...</div>;
+  if (fetching) {
+    return <div className="flex items-center justify-center h-screen -mt-36"><LoadingIndicator /></div>;
   }
 
   return (
